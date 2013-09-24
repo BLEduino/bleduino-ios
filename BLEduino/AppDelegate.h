7ec58425fcc12c2c317e60cfee3c717280841e4c
Ramon Gonzalez <rjgt04@gmail.com>
2013-09-24 19:24:42 -0400

    Initial Commit

---
+++ BLEduino/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BLEduino
+//
+//  Created by Ramon Gonzalez on 9/24/13.
+//  Copyright (c) 2013 Kytelabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
