f9e6d85335e4f2855024c5c91c37fcb4b8df0e40
Ramon Gonzalez <rjgt04@gmail.com>
2013-09-28 21:03:50 -0400

    UART Service class basically done.

--- BLEduino-Libray/UARTService.h
+++ BLEduino-Libray/UARTService.h
@@ -12,9 +12,9 @@
 /****************************************************************************/
 /*						Service & Characteristics							*/
 /****************************************************************************/
-extern NSString *kUARTServiceUUIDString;            //8C6B-DA7A-A312-681D-025B-0032-C0D1-6A2D  UART Service
-extern NSString *kRxCharacteristicUUIDString;       //8C6B-ABCD-A312-681D-025B-0032-C0D1-6A2D  Read(Rx) Message Characteristic
-extern NSString *kTxCharacteristicUUIDString;       //8C6B-1010-A312-681D-025B-0032-C0D1-6A2D  Write(Tx) Message Characteristic
+extern NSString *kUARTServiceUUIDString;            //8C6BDA7A-A312-681D-025B-0032C0D16A2D  UART Service
+extern NSString *kRxCharacteristicUUIDString;       //8C6BABCD-A312-681D-025B-0032C0D16A2D  Read(Rx) Message Characteristic
+extern NSString *kTxCharacteristicUUIDString;       //8C6B1010-A312-681D-025B-0032C0D16A2D  Write(Tx) Message Characteristic
 
 
 /****************************************************************************/
@@ -23,10 +23,10 @@ extern NSString *kTxCharacteristicUUIDString;       //8C6B-1010-A312-681D-025B-0
 @class UARTService;
 @protocol UARTServiceDelegate <NSObject>
 @required
-- (void)uartService:(UARTService *)service didReceiveMessage:(NSString *)message;
+- (void)uartService:(UARTService *)service didReceiveMessage:(NSString *)message error:(NSError *)error;
 
 @optional
-- (void)uartService:(UARTService *)service didWriteMessage:(NSString *)message;
+- (void)uartService:(UARTService *)service didWriteMessage:(NSString *)message error:(NSError *)error;
 - (void)didSubscribeToReceiveMessagesFor:(UARTService *)service;
 - (void)didUnsubscribeToReceiveMessagesFor:(UARTService *)service;
 @end
@@ -37,7 +37,8 @@ extern NSString *kTxCharacteristicUUIDString;       //8C6B-1010-A312-681D-025B-0
 /****************************************************************************/
 @interface UARTService : NSObject <CBPeripheralDelegate>
 
-- (id) initWithPeripheral:(CBPeripheral *)peripheral controller:(id<UARTServiceDelegate>)controller;
+- (id) initWithPeripheral:(CBPeripheral *)aPeripheral controller:(id<UARTServiceDelegate>)aController;
+- (void) dismissPeripheral;
 
 // Writing messages to BLEduino.
 - (void) writeMessage:(NSString *)message withAck:(BOOL)enabled;
@@ -45,10 +46,8 @@ extern NSString *kTxCharacteristicUUIDString;       //8C6B-1010-A312-681D-025B-0
 
 // Read/Receiving messages from BLEduino.
 - (void) readMessage;
-- (void) subscribeToReceiveMessagesWithAck:(BOOL)enabled;
-- (void) subscribeToReceiveMessages;
-- (void) unsubscribeToReceiveMessagesWithAck:(BOOL)enabled;
-- (void) unsubscribeToReiceiveMessages;
+- (void) subscribeToStartReceivingMessages;
+- (void) unsubscribeToStopReiceivingMessages;
 
 @property (nonatomic, strong) NSMutableString *sentMessage;
 @property (nonatomic, strong) NSMutableString *receivedMessage;
--- BLEduino-Libray/UARTService.m
+++ BLEduino-Libray/UARTService.m
@@ -8,6 +8,145 @@
 
 #import "UARTService.h"
 
+/****************************************************************************/
+/*						Service & Characteristics							*/
+/****************************************************************************/
+NSString *kUARTServiceUUIDString = @"8C6BDA7A-A312-681D-025B-0032C0D16A2D";
+NSString *kRxCharacteristicUUIDString = @"8C6BABCD-A312-681D-025B0032C0D16A2D";
+NSString *kTxCharacteristicUUIDString = @"8C6B1010-A312-681D-025B0032C0D16A2D";
+
+@interface UARTService() <CBPeripheralDelegate>
+{
+    @private
+    CBPeripheral		*servicePeripheral;
+    
+    CBService			*uartService;
+    CBCharacteristic    *rxCharacteristic;
+    CBCharacteristic	*txCharacteristic;
+
+    CBUUID              *uartServiceUUID;
+    CBUUID              *rxCharacteristicUUID;
+    CBUUID              *txCharacteristicUUID;
+    
+    id <UARTServiceDelegate> delegate;
+}
+@end
+
+
 @implementation UARTService
+@synthesize sentMessage, receivedMessage;
+@synthesize peripheral = servicePeripheral;
+
+#pragma mark -
+#pragma mark Init
+/****************************************************************************/
+/*								Init										*/
+/****************************************************************************/
+
+- (id) initWithPeripheral:(CBPeripheral *)aPeripheral
+               controller:(id<UARTServiceDelegate>)aController
+{
+    self = [super init];
+    if (self) {
+        servicePeripheral = aPeripheral;
+        servicePeripheral.delegate = self;
+		delegate = aController;
+        
+        uartServiceUUID = [CBUUID UUIDWithString:kUARTServiceUUIDString];
+        rxCharacteristicUUID = [CBUUID UUIDWithString:kRxCharacteristicUUIDString];
+        txCharacteristicUUID = [CBUUID UUIDWithString:kTxCharacteristicUUIDString];
+    }
+    
+    return self;
+}
+
+- (id) uartServiceWithController:(id<UARTServiceDelegate>)aController
+{
+    //PENDING
+    //selects peripheral automatically and abstracts the need to handle the peripheral completely.
+    return nil;
+}
+
+
+#pragma mark -
+#pragma mark Write Messages
+/****************************************************************************/
+/*				      Write messages to BLEduino                            */
+/****************************************************************************/
+//
+- (void) writeMessage:(NSString *)message withAck:(BOOL)enabled
+{
+    NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
+    
+    if(enabled)
+    {
+        [servicePeripheral writeValue:data forCharacteristic:rxCharacteristic type:CBCharacteristicWriteWithResponse];
+    }
+    else
+    {
+        [servicePeripheral writeValue:data forCharacteristic:rxCharacteristic type:CBCharacteristicWriteWithoutResponse];
+    }
+    
+}
+- (void) writeMessage:(NSString *)message
+{
+    [self writeMessage:message withAck:NO];
+}
+
+
+#pragma mark -
+#pragma mark Read Messages
+/****************************************************************************/
+/*				      Read messages from BLEduino                           */
+/****************************************************************************/
+- (void) readMessage
+{
+    [servicePeripheral readValueForCharacteristic:txCharacteristic];
+}
+- (void) subscribeToStartReceivingMessages
+{
+    [servicePeripheral setNotifyValue:YES forCharacteristic:txCharacteristic];
+}
+
+- (void) unsubscribeToStopReiceivingMessages
+{
+    [servicePeripheral setNotifyValue:NO forCharacteristic:txCharacteristic];
+}
+
+- (void) dismissPeripheral
+{
+	if (servicePeripheral) {
+		servicePeripheral = nil;
+        servicePeripheral.delegate = nil;
+	}
+}
+
+#pragma mark -
+#pragma mark Peripheral Delegate
+/****************************************************************************/
+/*				            Peripheral Delegate                             */
+/****************************************************************************/
+
+- (void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    [delegate uartService:self didWriteMessage:self.sentMessage error:error];
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    [delegate uartService:self didReceiveMessage:self.receivedMessage error:error];
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    if(characteristic.isNotifying)
+    {
+        [delegate didSubscribeToReceiveMessagesFor:self];
+    }
+    else
+    {
+        [delegate didUnsubscribeToReceiveMessagesFor:self];
+    }
+}
 
 @end
