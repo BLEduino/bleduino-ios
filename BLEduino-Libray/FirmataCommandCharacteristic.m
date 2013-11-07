664f7ad89a9ee1107da3d75d0610b02ea0bef03d
Ramon Gonzalez <rjgt04@gmail.com>
2013-11-07 09:34:30 -0500

    Finished firmata command characteristc class. Need to revised the data and init methods for ThrottleYawRollPitch characteistic. Also need to reivise its @property instance types

--- BLEduino-Libray/FirmataCommandCharacteristic.m
+++ BLEduino-Libray/FirmataCommandCharacteristic.m
@@ -15,7 +15,28 @@
  */
 - (id) initWithData:(NSData *)firmataCommandData
 {
-    return nil;
+    //Converting pinNumber byte to integer.
+    Byte *pinNumberByte = (Byte*)malloc(1);
+    NSRange pinNumberRange = NSMakeRange(0, 1);
+    [firmataCommandData getBytes:pinNumberByte range:pinNumberRange];
+    NSData *pinNumberData = [[NSData alloc] initWithBytes:pinNumberByte length:1];
+    self.pinNumber = *(int*)([pinNumberData bytes]);
+    
+    //Converting pinState byte to corresponding FirmataCommandPinState.
+    Byte *pinStateByte = (Byte*)malloc(1);
+    NSRange pinStateRange = NSMakeRange(1, 1);
+    [firmataCommandData getBytes:pinStateByte range:pinStateRange];
+    NSData *pinStateData = [[NSData alloc] initWithBytes:pinStateByte length:1];
+    self.pinState = *(int*)([pinStateData bytes]);
+
+    //Converting pinValue byte to integer.
+    Byte *pinValueByte = (Byte*)malloc(1);
+    NSRange pinValueRange = NSMakeRange(2, 1);
+    [firmataCommandData getBytes:pinValueByte range:pinValueRange];
+    NSData *pinValueData = [[NSData alloc] initWithBytes:pinValueByte length:1];
+    self.pinValue = *(int*)([pinValueData bytes]);
+    
+    return self;
 }
 
 /*
@@ -23,21 +44,21 @@
  */
 - (NSData *)data
 {
-//    NSMutableData *firmataCommandData = [[NSMutableData alloc] initWithCapacity:3];
-//    
-//    int pinSate = self.pinState;
-//    NSData *dataThrottle = [NSData dataWithBytes:&throttle length:sizeof(throttle)];
-//    [motionData appendData:dataThrottle];
-//    
-//    int pinNumber = [self.pinNumber floatValue];
-//    NSData *dataYaw = [NSData dataWithBytes:&yaw length:sizeof(yaw)];
-//    [motionData appendData:dataYaw];
-//    
-//    int pinValue = [self.pinValue floatValue];
-//    NSData *dataRoll = [NSData dataWithBytes:&roll length:sizeof(roll)];
-//    [motionData appendData:dataRoll];
-//    
-//    return firmataCommandData;
+    NSMutableData *firmataCommandData = [[NSMutableData alloc] initWithCapacity:3];
+    
+    Byte pinNumberByte = (self.pinNumber >> (0)) & 0xff;
+    NSMutableData *pinNumberData = [NSMutableData dataWithBytes:&pinNumberByte length:sizeof(pinNumberByte)];
+    [firmataCommandData appendData:pinNumberData];
+
+    Byte pinStateByte = (self.pinState >> (0)) & 0xff;
+    NSMutableData *pinStateData = [NSMutableData dataWithBytes:&pinStateByte length:sizeof(pinStateByte)];
+    [firmataCommandData appendData:pinStateData];
+    
+    Byte pinValueByte = (self.pinState >> (0)) & 0xff;
+    NSMutableData *pinValueData = [NSMutableData dataWithBytes:&pinValueByte length:sizeof(pinValueByte)];
+    [firmataCommandData appendData:pinValueData];
+    
+    return firmataCommandData;
     
     return nil;
 }
